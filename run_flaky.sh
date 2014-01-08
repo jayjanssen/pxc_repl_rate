c18c5849cb6d284175ed2b55cedad72ef768ba7e
Jay Janssen <jay.janssen@gmail.com>
2014-01-08 10:43:53 -0500

    fix up

--- run_flaky.sh
+++ run_flaky.sh
@@ -10,19 +10,18 @@ vagrant destroy -f
 ln -sf Vagrantfile-5.6 Vagrantfile
 vagrant up
 
-
 # baseline
 ./vagrant-percona/pxc-bootstrap.sh
 
 # Initialize test
 echo "Initializing sysbench"
-vagrant ssh ${nodes[0]} -c "/vagrant/tests/init_sysbench.sh"
+vagrant ssh node1 -c "/vagrant/tests/init_sysbench.sh"
 
 ./run_test.sh flakey-baseline 600
 
 # Gradually increase the packet loss
-#pkt_loss_pcts=( "0.01" "0.05" "0.10" "0.20" "0.50" )
-pkt_loss_pcts=( "0.50" )
+pkt_loss_pcts=( "0.01" "0.05" "0.10" "0.20" "0.50" )
+#pkt_loss_pcts=( "0.50" )
 
 for pkt_loss_pct in "${pkt_loss_pcts[@]}" 
 do
@@ -41,5 +40,5 @@ iptables -A OUTPUT -d 192.168.70.0/24 -m statistic --mode random --probability $
 	
 	# Initialize test
 	echo "Initializing sysbench"
-	vagrant ssh ${nodes[0]} -c "/vagrant/tests/init_sysbench.sh"
+	vagrant ssh node1 -c "/vagrant/tests/init_sysbench.sh"
 done
