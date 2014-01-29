62570cc299cbaeb3669519aacdd4bfba5bee5358
Jay Janssen <jay.janssen@gmail.com>
2014-01-29 08:55:46 -0500

    fixed up seed computation

--- tests/run_sysbench.sh
+++ tests/run_sysbench.sh
@@ -3,13 +3,10 @@
 echo "Sysbench run for $1 seconds"
 
 
-local_pxc_index=`mysql -e "show global status like 'wsrep_local_index'" --raw 
---skip-column-names --vertical| tail -n 1`
-current_unix_time=`mysql -e "select unix_timestamp()" --raw 
---skip-column-names --vertical| tail -n 1`
+local_pxc_index=`mysql -e "show global status like 'wsrep_local_index'" --raw --skip-column-names --vertical| tail -n 1`
+current_unix_time=`mysql -e "select unix_timestamp()" --raw --skip-column-names --vertical| tail -n 1`
 
-rand_seed=$(( ($current_unix_time / ($local_pxc_index +10 )) + 
-($current_unix_time % ($local_pxc_index + 10)) ))
+rand_seed=$(( ($current_unix_time / ($local_pxc_index +10 )) + ($current_unix_time % ($local_pxc_index + 10)) ))
 
 
 start_com_updates=`mysql -e "show global status like 'Com_update'" --raw --skip-column-names --vertical| tail -n 1`
@@ -18,7 +15,7 @@ start_wsrep_last_committed=`mysql -e "show global status like 'wsrep_last_commit
 start_wsrep_replicated=`mysql -e "show global status like 'wsrep_replicated'" --raw --skip-column-names --vertical| tail -n 1`
 start_wsrep_replicated_bytes=`mysql -e "show global status like 'wsrep_replicated_bytes'" --raw --skip-column-names --vertical| tail -n 1`
 
-sysbench --db-driver=mysql --test=sysbench_tests/db/update_non_index.lua --rand-type=uniform --rand-seed=$rand-seed --mysql-user=test --mysql-password=test --mysql-db=test --oltp-table-size=250000 --max-requests=0 --num-threads=8 --max-time=$1 --report-interval=1 run
+sysbench --db-driver=mysql --test=sysbench_tests/db/update_non_index.lua --rand-type=uniform --rand-seed=$rand_seed --mysql-user=test --mysql-password=test --mysql-db=test --oltp-table-size=250000 --max-requests=0 --num-threads=8 --max-time=$1 --report-interval=1 run
 
 end_com_updates=`mysql -e "show global status like 'Com_update'" --raw --skip-column-names --vertical| tail -n 1`
 end_inno_updates=`mysql -e "show global status like 'Innodb_rows_updated'" --raw --skip-column-names --vertical| tail -n 1`
